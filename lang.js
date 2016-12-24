7d479fe8378c06e7e57880a4c5c82fa0f2576c5c
Alexey Velikiy <gmpota@gmail.com>
2016-12-24 20:49:13 +0300

    add lang

---
+++ lang.js
@@ -0,0 +1,14 @@
+var Lang = {}
+Lang.res.en = {
+  "editorial.title" : "Enter your text",
+  "editorial.span" : "at most 40 symbols",
+  "ui.save" : "save",
+  "ui.edit" : "edit",
+}
+
+Lang.res.en = {
+  "editorial.title" : "Введите текст поздравления",
+  "editorial.span" : "ограничение 40 символов",
+  "ui.save" : "сохранить",
+  "ui.edit" : "редактировать",
+}
