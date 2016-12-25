bab7243ad8f5f694efe732631f4d5195a43f4b9c
Alexey Velikiy <gmpota@gmail.com>
2016-12-25 22:49:14 +0300

    shr txt

--- lang.js
+++ lang.js
@@ -5,6 +5,7 @@ Lang.res.en = {
   "editorial.span" : "at most 40 symbols",
   "ui.save" : "SAVE AND PUBLISH",
   "ui.edit" : "CREATE YOUR OWN",
+  "share.text": "Polytechnic Museum sent you a snowy card"
 }
 
 Lang.res.ru = {
@@ -14,19 +15,34 @@ Lang.res.ru = {
   "editorial.span" : "ограничение 40 символов",
   "ui.save" : "СОХРАНИТЬ И ОПУБЛИКОВАТЬ",
   "ui.edit" : "СОЗДАТЬ СВОЕ",
+  "share.text": "Политехнический музей шлет тебе снежную открытку"
 }
 
 var language = window.navigator.userLanguage || window.navigator.language;
 
 Lang.local = Lang.res.en;
 
-if(language.toLocaleLowerCase().indexOf('ru') > -1) {
-  Lang.local = Lang.res.ru;
-  document.title = Lang.local['meta.title'];
+function setDescription(text) {
   var meta=document.getElementsByTagName("meta");
   for (var i=0; i<meta.length; i++) {
     if (meta[i].name.toLowerCase()=="description") {
-      meta[i].content = Lang.local['meta.description'];
+      meta[i].content = text;
     }
   }
+}
+
+function getDescription(text) {
+  var meta=document.getElementsByTagName("meta");
+  for (var i=0; i<meta.length; i++) {
+    if (meta[i].name.toLowerCase()=="description") {
+      return meta[i].content;
+    }
+  }
+}
+
+
+if(language.toLocaleLowerCase().indexOf('ru') > -1) {
+  Lang.local = Lang.res.ru;
+  document.title = Lang.local['meta.title'];
+  setDescription(Lang.local['meta.description'])
 }
